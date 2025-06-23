04abcc75672b7d8c7637fb6c283115cb655eb026
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2025-06-23 10:21:48 -0400

    lint/format

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-import { ThemeProvider } from "next-themes"
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ThemeProvider } from "next-themes";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ChakraProvider value={defaultSystem}>
-      <ThemeProvider attribute="class" disableTransitionOnChange>
-        <App />
-      </ThemeProvider>
-    </ChakraProvider>
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<ChakraProvider value={defaultSystem}>
+			<ThemeProvider attribute="class" disableTransitionOnChange>
+				<App />
+			</ThemeProvider>
+		</ChakraProvider>
+	</React.StrictMode>,
+);
